a1cfb95ca9e417cca08022dc5b4f420f062848b0
Barnabas27 <bnyambura032@gmail.com>
2021-01-27 00:22:19 +0300

    add function that renders the home page,allows user to register,show and update their profile,post their projects.

--- trophy/views.py
+++ trophy/views.py
@@ -14,103 +14,125 @@ from rest_framework import status
 from .permissions import IsAdminOrReadOnly
 from rest_framework import authentication, permissions
 
-def index(request):
-    date = dt.date.today()
-    heading = 'Welcome to Barbez Accolade App'
+@login_required(login_url='/accounts/login/')
+def home(request):
     projects = Projects.objects.all()
-    
-    return render(request, 'home.html', {"date": date, "heading":heading, "projects":projects})
+    context = {
+    "projects":projects,
+    }
+    return render(request, 'index.html', locals())
 
-def get_project_by_id(request, id):
 
-    try:
-        project = Projects.objects.get(pk = id)
-        
-    except ObjectDoesNotExist:
-        raise Http404()    
-    
-    return render(request, "project.html", {"project":project})
+def registration(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'registration/register.html', context)
+
 
 @login_required(login_url='/accounts/login/')
-def new_project(request):
+def profile(request):
+    projects = Projects.objects.all()
+    posts = Profile.objects.all()
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been successfully updated')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+    'user_form':user_form,
+    'profile_form':profile_form,
+    'posts':posts,
+    'projects':projects,
+    }
+    return render(request, 'profile/profile.html', context)
+
+
+@login_required(login_url='/accounts/login/')
+def updateprofile(request):
+    projects = Projects.objects.all()
+    posts = Profile.objects.all()
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been successfully updated')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+    'user_form':user_form,
+    'profile_form':profile_form,
+    'posts':posts,
+    'projects':projects,
+    }
+
+    return render(request, 'profile/update_profile.html', context)
+
+
+@login_required(login_url='/accounts/login/')
+def postproject(request):
     current_user = request.user
     if request.method == 'POST':
-        form = NewProjectForm(request.POST, request.FILES)
+        form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
             project = form.save(commit=False)
             project.author = current_user
             project.save()
-        return redirect('index')
-
+        return redirect('/')
     else:
-        form = NewProjectForm()
-    return render(request, 'new_project.html', {"form":form})
+        form = ProjectForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'PostProject.html', context)
 
-class ProjectsList(APIView):
-    def get(self, request, format=None):
-        all_merch = Projects.objects.all()
-        serializers = ProjectsSerializer(all_merch, many=True)
-        return Response(serializers.data)
-    
-class ProjectsDescription(APIView):
-    permission_classes = (IsAdminOrReadOnly,)
-    def get_projects(self, pk):
-        try:
-            return Projects.objects.get(pk=pk)
-        except Projects.DoesNotExist:
-            return Http404
+@login_required(login_url='/accounts/login/')
+def get_project(request, id):
+    project = Projects.objects.get(pk=id)
+
+    return render(request, 'project.html', {'project':project})
+
+@login_required(login_url='/accounts/login/')
+def search_projects(request):
+    if 'project' in request.GET and request.GET['project']:
+        search_term = request.GET["project"]
+        searched_projects = Projects.search_projects(search_term)
+        message = f"{search_term}"
         
-    def get(self, request, pk, format=None):
-        project = self.get_projects(pk)
-        serializers = ProjectsSerializer(project)
-        return Response(serializers.data)
-    
-    def put(self, request, pk, format=None):
-        project = self.get_projects(pk)
-        serializers = ProjectsSerializer(project, request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data)
-        else:
-            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk, format=None):
-        project = self.get_projects(pk)
-        project.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
+        return render(request, 'search.html', {"message":message, "projects": searched_projects})
+    else:
+        message = "You haven't searched for any user"
+
+        return render(request, 'search.html', {"message":message})
+
 class ProfileList(APIView):
     def get(self, request, format=None):
         all_merch = Profile.objects.all()
         serializers = ProfileSerializer(all_merch, many=True)
         return Response(serializers.data)
 
-@login_required(login_url='/accounts/login/')
-def search_projects(request):
-    if 'keyword' in request.GET and request.GET["keyword"]:
-        search_term = request.GET.get("keyword")
-        searched_projects = Projects.search_projects(search_term)
-        message = f"{search_term}"
-
-        return render(request, 'search.html', {"message":message,"projects": searched_projects})
-
-    else:
-        message = "You haven't searched for any term"
-        return render(request, 'search.html', {"message": message})
-    
-@login_required(login_url='/accounts/login/')
-def user_profiles(request):
-    current_user = request.user
-    author = current_user
-    projects = Projects.get_by_author(author)
-    
-    if request.method == 'POST':
-        form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.photo)
-        if form.is_valid():
-            profile = form.save(commit=False)
-            profile.save()
-        return redirect('profile')
-        
-    else:
-        form = ProfileUpdateForm()    
-    return render(request, 'django_registration/profile.html', {"form":form, "projects":projects})
+class ProjectsList(APIView):
+    def get(self, request, format=None):
+        all_merch = Projects.objects.all()
+        serializers = ProjectSerializer(all_merch, many=True)
+        return Response(serializers.data)
